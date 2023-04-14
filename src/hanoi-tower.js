caf44de537e1af09aee80ff9af3d19889375e50d
ChessMasterr <artiom.farhutdinoff@yandex.ru>
2023-04-14 13:49:34 +0500

    feat: 2 more answers 14 апр 2023 г. 13:49:33

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,24 +1,31 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const hanoiResult = {
+    turns: 0,
+    seconds: 0,
+  };
+  hanoiResult.turns = 2 ** disksNumber - 1;
+  hanoiResult.seconds = Math.floor((3600 / turnsSpeed) * hanoiResult.turns);
+  return hanoiResult;
 }
 
 module.exports = {
-  calculateHanoi
+  calculateHanoi,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,51 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (date === null || date === undefined) {
+    return "Unable to determine the time of year!";
+  }
+  // if(((date.getMonth() === 0) || (date.getMonth() === 2) || (date.getMonth() === 4) || (date.getMonth() === 6) || (date.getMonth() === 7) || (date.getMonth() === 9) || (date.getMonth() === 11)) && (date.getDate() > 31)) {
+  //   throw new Error("Invalid date!")
+  // }
+  // if((date.getMonth() === 2) && (date.getDate() > 29)) {
+  //   throw new Error("Invalid date!")
+  // }
+  // if(((date.getMonth() === 3) || (date.getMonth() === 5) || (date.getMonth() === 8) || (date.getMonth() === 10)) && (date.getDate() > 30)) {
+  //   throw new Error("Invalid date!")
+  // }
+  // if(!(date instanceof (Date)) || date.hasOwnProperty("toString")){
+  //   throw new Error("Invalid date!")
+  // }
+  if (!(date instanceof Date) || date.hasOwnProperty("toString")) {
+    throw new Error("Invalid date!");
+  }
+  if (date.getMonth() == 0 || date.getMonth() == 1 || date.getMonth() == 11) {
+    return "winter";
+  }
+  if (date.getMonth() == 2 || date.getMonth() == 3 || date.getMonth() == 4) {
+    return "spring";
+  }
+  if (date.getMonth() == 5 || date.getMonth() == 6 || date.getMonth() == 7) {
+    return "summer";
+  }
+  if (date.getMonth() == 8 || date.getMonth() == 9 || date.getMonth() == 10) {
+    return "autumn";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
