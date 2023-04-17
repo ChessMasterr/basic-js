3dcbd218a73186c7099a633259acfb5cf86fc17b
ChessMasterr <artiom.farhutdinoff@yandex.ru>
2023-04-17 23:55:05 +0500

    feat: saper 17 апр 2023 г. 23:55:05

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-}
+  let result = [];
+
+  for(let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for(let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+      if(matrix[i-1] && matrix[i-1][j]) {
+        count++;
+      };
+      if(matrix[i-1] && matrix[i-1][j+1]) {
+        count++;
+      };
+      if(matrix[i] && matrix[i][j+1]) {
+        count++;
+      };
+      if(matrix[i+1] && matrix[i+1][j+1]) {
+        count++;
+      };
+      if(matrix[i+1] && matrix[i+1][j]) {
+        count++;
+      };
+      if(matrix[i+1] && matrix[i+1][j-1]) {
+        count++;
+      };
+      if(matrix[i] && matrix[i][j-1]) {
+        count++;
+      };
+      if(matrix[i-1] && matrix[i-1][j-1]) {
+        count++;
+      };
+      result[i].push(count);
+    }
+  }
+  return result;
+    }
+
 
 module.exports = {
   minesweeper
